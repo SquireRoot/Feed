9904f3cc9124bde2dee940205be8b8d678f98cc5
Martin Garin <ggarin@gmail.com>
2017-07-29 03:37:50 -0400

    no message

--- Feed/app/src/main/java/com/bogus/evannewman/feed/Post.java
+++ Feed/app/src/main/java/com/bogus/evannewman/feed/Post.java
@@ -1,5 +1,10 @@
 package com.bogus.evannewman.feed;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.lang.Exception;
+import java.lang.System;
 import java.net.URL;
 
 /**
@@ -11,19 +16,35 @@ public class Post {
     private String id;
     private String title;
     private URL imageURL;
-    private URL thumbnailURL;
+    private Bitmap image;
+    private URL error = new URL("http://www.freeiconspng.com/uploads/round-error-icon-16.jpg");
     // image
 
 
-    public Post() {
-
+    public Post(String id, String title, String url) {
+        this.imageURL = new URL(url);
+        this.id = id;
+        this.title = title;
     }
 
     public boolean loadData() {
         // load the image into cache
+        try {
+            image = BitmapFactory.decodeStream(imageURL.openStream().getInputStream);
+            //use a network function
+        }catch(Exception e){
+            image = BitmapFactory.decodeStream(error.openStream().getInputStream);
+
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+        return true;
     }
 
     public boolean unloadData() {
         // delete image cache
+        image = null;
+        return true;
     }
 }
