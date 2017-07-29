bb7458a1c7a4c538b54ce7a805245dcc55f9cf50
Martin Garin <ggarin@gmail.com>
2017-07-29 01:51:23 -0400

    Layouts

--- Feed/app/src/main/java/com/bogus/evannewman/feed/Main.java
+++ Feed/app/src/main/java/com/bogus/evannewman/feed/Main.java
@@ -12,5 +12,6 @@ public class Main extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_feed);
     }
 }
