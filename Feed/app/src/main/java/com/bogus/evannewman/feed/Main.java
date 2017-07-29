31f535f4df537091592b28812a0dcac4d9582cbb
Unknown <evanmnewman1@gmail.com>
2017-07-29 00:57:18 -0400

    Added Android Studio project for real this time

---
+++ Feed/app/src/main/java/com/bogus/evannewman/feed/Main.java
@@ -0,0 +1,16 @@
+package com.bogus.evannewman.feed;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Evan Newman on 7/29/2017.
+ */
+
+public class Main extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ Feed/app/src/main/java/com/bogus/evannewman/feed/Post.java
@@ -0,0 +1,27 @@
+package com.bogus.evannewman.feed;
+
+import java.net.URL;
+
+/**
+ * Created by Evan Newman on 7/29/2017.
+ */
+
+public class Post {
+    // post title
+    private String title;
+    // image url
+    URL imageURL;
+    // image
+
+    public Post() {
+
+    }
+
+    public loadData() {
+        // load the image into cache
+    }
+
+    public unloadData() {
+        // delete image cache
+    }
+}
